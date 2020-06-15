8873ead9ebcc6a6d2792781f59ecaa2456f7e412
LukaszGalinski <galczo230@gmail.com>
2020-06-15 13:34:01 +0200

    Added loading and passing json data, coded in base64, into the recyclerView

---
+++ app/src/main/java/com/lukaszgalinski/gamefuture/HttpHandler.kt
@@ -0,0 +1,62 @@
+package com.lukaszgalinski.gamefuture
+
+import android.content.Context
+import android.util.Base64
+import android.util.Log
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.ProtocolException
+import java.net.URL
+
+private const val TAG: String = "HttpHandler:"
+private const val GET_METHOD = "GET"
+private const val CODING_SYSTEM = "UTF-8"
+class HttpHandler(private val context: Context){
+
+    fun makeServiceCall(reqUrl: String): String{
+        var response = ""
+        try {
+            val url = URL(reqUrl)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = GET_METHOD
+            val inputStream: InputStream = BufferedInputStream(connection.inputStream)
+            response = streamToString(inputStream)
+        } catch (e: MalformedURLException) {
+            Log.e(TAG,  context.resources.getString(R.string.loading_malformedException) + e.message);
+        } catch (e: ProtocolException) {
+            Log.e(TAG, context.resources.getString(R.string.loading_protocolException)  + e.message);
+        } catch (e: IOException) {
+            Log.e(TAG, context.resources.getString(R.string.loading_ioException)  + e.message);
+        } catch (e: Exception) {
+            Log.e(TAG, context.resources.getString(R.string.loading_exception) + e.message);
+        }
+        return response
+    }
+
+    private fun streamToString(input: InputStream): String {
+        val reader = BufferedReader(InputStreamReader(input))
+        val sb = StringBuilder()
+        var line: String?
+        try {
+            while (reader.readLine().also { line = it } != null) {
+                sb.append(line)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            try {
+                input.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        return decodeData(sb)
+    }
+
+    private fun decodeData(stringBuilder: StringBuilder): String{
+        val data = Base64.decode(stringBuilder.toString(), Base64.DEFAULT)
+        return String(data, charset(CODING_SYSTEM))
+    }
+}
--- app/src/main/java/com/lukaszgalinski/gamefuture/MainMenuActivity.kt
+++ app/src/main/java/com/lukaszgalinski/gamefuture/MainMenuActivity.kt
@@ -1,27 +1,33 @@
 package com.lukaszgalinski.gamefuture
 
+import android.content.Context
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.main_menu_layout.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
+private const val TAG: String = "MainMenuActivity "
+private const val URL = "https://pastebin.com/raw/0DEX1tc2"
+private const val JSON_ARRAY_LABEL = "places"
+private val arrayList = ArrayList<GamesData?>()
+lateinit var adapter: GamesListAdapter
 class MainMenuActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu_layout)
-        val value = GamesData("numer", "nic", "http")
-        val arrayList: ArrayList<GamesData> = ArrayList()
-        arrayList.add(value)
-        arrayList.add(value)
-        arrayList.add(value)
-        arrayList.add(value)
-        arrayList.add(value)
-        arrayList.add(value)
-        arrayList.add(value)
+        GetGames(this).execute()
+        showData(arrayList)
+    }
 
-
-        val adapter = GamesListAdapter(this, arrayList)
+    private fun showData(arrayList: ArrayList<GamesData?>){
+         adapter = GamesListAdapter(this, arrayList)
         val mLayoutManager: RecyclerView.LayoutManager = GridLayoutManager(this, 2)
         menu_recycler.adapter = adapter
         menu_recycler.apply {
@@ -29,3 +35,41 @@ class MainMenuActivity: AppCompatActivity() {
         }
     }
 }
+
+class GetGames(private val context: Context): AsyncTask<Void, Void, Void>() {
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+        Toast.makeText(context, context.resources.getString(R.string.loading_starting), Toast.LENGTH_LONG).show()
+    }
+
+    override fun doInBackground(vararg params: Void?): Void? {
+        val httpHandler = HttpHandler(context)
+        val jsonStr = httpHandler.makeServiceCall(URL)
+        try {
+            val jsonObj: JSONObject = JSONObject(jsonStr)
+            val gamesArray: JSONArray = jsonObj.getJSONArray(JSON_ARRAY_LABEL)
+            arrayList.clear()
+            for (i in 0 until gamesArray.length()) {
+                val currentGame: JSONObject = gamesArray.getJSONObject(i)
+                val name = currentGame.getString("name")
+                val element = GamesData(name, "none", "none")
+                arrayList.add(element)
+            }
+
+        } catch (e: JSONException) {
+            Log.e(TAG, context.resources.getString(R.string.data_parsing_error) + e.message)
+        }
+
+        return null
+    }
+
+    override fun onPostExecute(result: Void?) {
+        super.onPostExecute(result)
+        Toast.makeText(context, context.resources.getString(R.string.loading_finished), Toast.LENGTH_LONG).show()
+        adapter.notifyDataSetChanged()
+    }
+}
+
+
+
